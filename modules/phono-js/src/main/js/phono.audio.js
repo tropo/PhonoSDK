1a0fe903bfb8e682963d6f546162654cfaa3aa03
Neil Stratford <nstratford@Neil-Stratfords-MacBook-Pro.local>
2011-07-21 20:05:54 +0100

    Add android support.

--- modules/phono-js/src/main/js/phono.audio.js
+++ modules/phono-js/src/main/js/phono.audio.js
@@ -1,7 +1,8 @@
 ;(function() {
     //@Include=phono.as-audio.js
     //@Include=phono.java-audio.js
-    //@Include=phono.phonegap-audio.js
+    //@Include=phono.phonegap-ios-audio.js
+    //@Include=phono.phonegap-android-audio.js
 
     Phono.registerPlugin("audio", {
         
@@ -13,15 +14,19 @@
             // What are we going to create? Look at the config...
             if (config.type === "java") {
                 return new JavaAudio(phono, config, callback);
-            } else if (config.type === "phonegap") {
-                return new PhonegapAudio(phono, config, callback);
+            } else if (config.type === "phonegap-ios") {
+                return new PhonegapIOSAudio(phono, config, callback);
+            } else if (config.type === "phonegap-android") {
+                return new PhonegapAndroidAudio(phono, config, callback);
             } else if (config.type === "flash") {
                 return new FlashAudio(phono, config, callback);
             } else if (config.type === "none") {
                 window.setTimeout(callback,10);
                 return null;
             } else if (config.type === "auto") {
-                if (PhonegapAudio.exists()) return new PhonegapAudio(phono, config, callback);
+                if (JavaAudio.exists()) return new JavaAudio(phono, config, callback);
+                else if (PhonegapIOSAudio.exists()) return new PhonegapIOSAudio(phono, config, callback);
+                else if (PhonegapAndroidAudio.exists()) return new PhonegapAndroidAudio(phono, config, callback);
                 else return new FlashAudio(phono, config, callback);
             }
         }
