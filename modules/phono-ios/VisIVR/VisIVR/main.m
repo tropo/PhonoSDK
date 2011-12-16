49c2b71972bc3a270e36e8037fb9eaac2af5d6d6
Tim Panton <tim@phonefromhere.com>
2011-12-16 20:40:18 +0000

    initial add

---
+++ modules/phono-ios/VisIVR/VisIVR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VisIVR
+//
+//  Created by Tim Panton on 15/12/2011.
+//  Copyright (c) 2011 Westhhawk Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VisIVRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VisIVRAppDelegate class]));
+    }
+}
