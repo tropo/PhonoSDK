5b0da1cc899fdab8e68a4f3df658faa5b8cb1ba4
Tim Panton <tim@phonefromhere.com>
2012-07-03 19:39:46 +0100

    Check in of SRTP code. Does _not_ work (yet)

---
+++ modules/phono-phonegap-ios/libphono/src/rtp/phonoSRTP.h
@@ -0,0 +1,13 @@
+//
+//  phonoSRTP.h
+//  libphono
+//
+//  Created by Tim Panton on 02/07/2012.
+//  Copyright (c) 2012 Voxeo. All rights reserved.
+//
+
+#import "phonoRTP.h"
+
+@interface phonoSRTP : phonoRTP
+
+@end
---
+++ modules/phono-phonegap-ios/libphono/src/rtp/phonoSRTP.m
@@ -0,0 +1,13 @@
+//
+//  phonoSRTP.m
+//  libphono
+//
+//  Created by Tim Panton on 02/07/2012.
+//  Copyright (c) 2012 Voxeo. All rights reserved.
+//
+
+#import "phonoSRTP.h"
+
+@implementation phonoSRTP
+
+@end
