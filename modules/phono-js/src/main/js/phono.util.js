3090e666826ef50766d532bdfdbe3d29050356d7
Your Name <neils@belltower.co.uk>
2012-08-16 11:39:29 +0100

    Fix for IE9. Move sendFnc into xhr object. [PHONO-70]

--- modules/phono-js/src/main/js/phono.util.js
+++ modules/phono-js/src/main/js/phono.util.js
@@ -210,6 +210,18 @@
         }
         return false;
     },
+    getIEVersion: function() {
+        var rv = -1; // Return value assumes failure.
+        if (navigator.appName == 'Microsoft Internet Explorer')
+        {
+            var ua = navigator.userAgent;
+            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+            if (re.exec(ua) != null)
+                rv = parseFloat( RegExp.$1 );
+        }
+        console.log("IE Version = " + rv);
+        return rv;
+    },
     localUri : function(fullUri) {
         var splitUri = fullUri.split(":");
         return splitUri[0] + ":" + splitUri[1] + ":" + splitUri[2];
