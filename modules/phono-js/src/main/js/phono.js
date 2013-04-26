f06f93be40a13d1765e7e923182073dfc66ca6b1
Neil Stratford <neils@belltower.co.uk>
2013-04-26 11:29:20 +0100

    Rework connection state machine to support IE.

--- modules/phono-js/src/main/js/phono.js
+++ modules/phono-js/src/main/js/phono.js
@@ -14,6 +14,9 @@ function Phono(config) {
 
     // Bind 'on' handlers
     Phono.events.bind(this, config);
+
+    // Wrap ourselves with logging
+    Phono.util.loggify("Phono", this);
    
     if(!config.apiKey) {
         this.config.apiKey = prompt("Please enter your Phono API Key.\n\nTo get a new one sign up for a free account at: http://www.phono.com");
@@ -29,7 +32,7 @@ function Phono(config) {
     // Initialize Fields
     this.sessionId = null;
     this.connTimers = [];
-    Phono.log.debug("ConnectionUrl: " + this.config.connectionUrl);
+    Phono.log.debug("[CONFIG] ConnectionUrl: " + this.config.connectionUrl);
 
     if(navigator.appName.indexOf('Internet Explorer')>0){
         xmlSerializer = {};
@@ -49,7 +52,33 @@ function Phono(config) {
         Strophe.pres = $pres;
         this.connection = this.config.connection;
     } else {
-        Phono.log.debug("adding loadbalancer");
+        // We need to make a connection
+        var phono = this;
+        var cfunc = function(curl) {
+            if (!phono.connected()) { 
+                Phono.log.debug("trying connection URL "+curl);
+                if (phono.connection != null){
+                    phono.connection.disconnect();
+                }
+                phono.connection = new Strophe.Connection(curl);
+                
+                phono.connection.xmlInput = function (body) {
+                    Phono.log.debug("[WIRE] (i) " + xmlSerializer.serializeToString(body));
+                };
+                
+                phono.connection.xmlOutput = function (body) {
+                Phono.log.debug("[WIRE] (o) " + xmlSerializer.serializeToString(body));
+                };
+                
+                phono.connect();
+            } else {
+            Phono.log.debug("[LB] already connected... not trying URL "+curl);
+            }
+        }  
+        
+        Phono.log.debug("[LB] Invoke loadbalancer");
+        // Create a dummy object so that the Strophe plugins get loaded
+        var dummy = connection = new PhonoStrophe.Connection(this.config.connectionUrl);
         var a="",b= function (){} ,c="";
         var sr = new PhonoStrophe.Request(a,b,c,0);
         var srvreq = sr.xhr;
@@ -57,7 +86,7 @@ function Phono(config) {
         var uri = document.createElement('a');
         var srv = "_phono";
         uri.href = this.config.connectionUrl
-        Phono.log.debug("OrigT ="+uri.hostname+" path ="+uri.pathname);
+        Phono.log.debug("[LB] OrigT ="+uri.hostname+" path ="+uri.pathname);
         if(uri.protocol == "https:"){
            srv = srv+"s";
         }
@@ -68,73 +97,57 @@ function Phono(config) {
             srvreq.overrideMimeType("application/json");
         }
         try {
-            srvreq.send(null);
-            if (srvreq.readyState == 4){
-                Phono.log.debug("got reply from loadbalancer")
-                if (srvreq.status == 200){
-                    if (srvreq.getResponseHeader("Content-Type")== "application/json"){
-                        Phono.log.debug("loadbalancer reply was "+srvreq.responseText);
+            // Set the ready state handler
+            srvreq.onreadystatechange = function() {
+                if (srvreq.readyState == 4) {
+                    Phono.log.debug("[LB] Got reply :" + srvreq.status)
+                    if (srvreq.status == 200) {
+                        Phono.log.debug("[LB] Reply was "+srvreq.responseText);
                         var srv = eval('(' +srvreq.responseText+ ')');
                         for (var s in srv.servers) {
                             var nexts = srv.servers[s];
-                            Phono.log.debug("nexts= "+nexts);
-
                             var curl = "";
                             // if the target matches the original connectionURL then use the path from that
                             // otherwise just append /http-bind
-                            if (uri.hostname == nexts.target){
-                                curl = uri.protocol+"//"+nexts.target +":"+nexts.port+uri.pathname;
+                            var path = uri.pathname;
+                            if (uri.hostname == nexts.target) {
+                                if (path.indexOf('/') != 0) path = "/" + path; 
                             } else {
-                                curl = uri.protocol+"//"+nexts.target +":"+nexts.port+"/http-bind";
+                                path = "/http-bind";
                             }
-                            Phono.log.debug("adding connection URL "+curl);
-                            curls.push (curl);
+                            curl = uri.protocol+"//"+nexts.target +":"+nexts.port+path;
+                            if (typeof nexts.target != 'undefined') {
+                                Phono.log.debug("[LB] Adding connection URL "+curl);
+                                curls.push(curl);
+                            }
+                        }
+                        Phono.log.debug("[LB] Adding default connection URL "+phono.config.connectionUrl);
+                        curls.push(phono.config.connectionUrl);
+                        Phono.log.debug("[LB] Initial connection URL "+curls[0]);        
+                        
+                        // add timers for all possible srv entries (and default)
+                        // if any work, we will skip the rest
+                        var t = 0;
+                        for (var c in curls) {
+                            if (curls[c].substring) {
+                                setTimeout(function(){if (curls[t].substring) cfunc(curls[t]);t = t+1;},20+(c*10000));
+                            }
                         }
                     } else {
-                        Phono.log.debug("loadbalancer response content type was " + srvreq.getResponseHeader("Content-Type"));
+                        Phono.log.debug("[LB] loadbalancer status was "+srvreq.status);
+                        Phono.log.debug("[LB] Using default connection URL "+phono.config.connectionUrl);
+                        cfunc(phono.config.connectionUrl);
                     }
-                } else {
-                    Phono.log.debug("loadbalancer status was "+srvreq.status);
                 }
             }
+            // Send the request
+            srvreq.send(null);
         } catch (e) {
-            Phono.log.debug("ignoring a loadbalance error "+e);
+            Phono.log.debug("[LB] error - ignoring a loadbalance error "+e);
+            Phono.log.debug("[LB] Using default connection URL "+phono.config.connectionUrl);
+            cfunc(phono.config.connectionUrl);
         }
-        Phono.log.debug("adding default connection URL "+this.config.connectionUrl);
-        curls.push(this.config.connectionUrl);
-        Phono.log.debug("initial connection URL "+curls[0]);
     } 
-
-    // Wrap ourselves with logging
-    Phono.util.loggify("Phono", this);
-
-    var phono = this;
-    var cfunc = function(curl){
-        if (!phono.connected()){
-            Phono.log.debug("trying connection URL "+curl);
-            if (phono.connection != null){
-                phono.connection.disconnect();
-            }
-            phono.connection = new Strophe.Connection(curl);
-            
-            phono.connection.xmlInput = function (body) {
-                Phono.log.debug("[WIRE] (i) " + xmlSerializer.serializeToString(body));
-            };
-            
-            phono.connection.xmlOutput = function (body) {
-                Phono.log.debug("[WIRE] (o) " + xmlSerializer.serializeToString(body));
-            };
-
-            phono.connect();
-        } else {
-            Phono.log.debug("already connected... not trying URL "+curl);
-        }
-    }  
-    // add timers for all possible srv entries (and default)
-    // if any work, we will skip the rest
-    for (var c in curls ){
-        setTimeout(cfunc,20+(c*10000),curls[c]);
-    }
 };
 
 (function() {
@@ -297,7 +310,7 @@ function Phono(config) {
     // ======================================================================
 
     PhonoStrophe.log = function(level, msg) {
-        Phono.log.debug("[PHONOSTROPHE] " + msg);
+        Phono.log.debug("[PSTROPHE] " + msg);
     };
 
     // Register Loggign Callback
